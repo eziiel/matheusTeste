0be7ced3cb8b40b8da50bafaaf839e364f750863
eziiel <suportesinop32@gbonline.com.br>
2025-04-08 10:37:11 -0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './app.css'
-import Routes2 from './router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Routes2 from "./router";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Routes2 />
-  </StrictMode>,
-)
+  </StrictMode>
+);
